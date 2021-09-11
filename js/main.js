8abbfb7455fe1d6ecd6892dfb95ae8e78949c804
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-09-11 12:35:56 -0300

    Inclusão do HTML e JS.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Criar uma função que exclua os números ímpares de um array
+//Dividindo o problema em partes:
+//Tenho que filtrar este array e em seguida verificar os números que são ímpares e excluí-los.
+ function deleteOdd(listHead) {
+     return listHead[i] % 2 == 0;
+     var filtred = [1,2,3,4,5,6,7,8,9,10].filter(deleteOdd);
+
+     console.log(filtred);
+     
+} 
+
+//Exemplo na Net
+ /* var numbers = [1,2,3,4,5,6,7,8];
+
+ var evenNumbers = numbers.filter(function(item) {
+      return(item % 2 == 0);
+ });
+
+ console.log(evenNumbers); */
+
+
+
+
