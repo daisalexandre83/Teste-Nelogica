bb784fe0a2adf2f74b972837ebdf6374103127a5
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-09-13 22:06:21 -0300

    Alterações na função do Javascript.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
+
 //Criar uma função que exclua os números ímpares de um array
 //Dividindo o problema em partes:
 //Tenho que filtrar este array e em seguida verificar os números que são ímpares e excluí-los.
  function deleteOdd(listHead) {
-     return listHead[i] % 2 == 0;
-     var filtred = [1,2,3,4,5,6,7,8,9,10].filter(deleteOdd);
+   
+   let pair = listHead.filter( index => index % 2 == 0) ;
 
-     console.log(filtred);
-     
+
+  console.log(pair);
 } 
+
+ let listHead = [1,2,3,4,5,6,7,8,9,10];
+
+deleteOdd(listHead);
+
+ 
 
 //Exemplo na Net
  /* var numbers = [1,2,3,4,5,6,7,8];
